53ebdb8c472faf13ec9ce155c4e0bd7c3dd9a9de
istvanjuhasz1212 <istvanjuhasz1212@gmail.com>
2025-05-04 14:10:28 +0200

    Created Frontend/Backend

---
+++ BACKEND/KOLTSEGVETESIELEMZO_S7VVH5_JUHASZ/KOLTSEGVETESIELEMZO_S7VVH5_JUHASZ/Program.cs
@@ -0,0 +1,15 @@
+namespace KOLTSEGVETESIELEMZO_S7VVH5_JUHASZ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
